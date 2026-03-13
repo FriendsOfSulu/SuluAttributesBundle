0a05fead84670249fd76cb8240b4cc48e8801d2a
mamazu <14860264+mamazu@users.noreply.github.com>
2026-03-13 15:47:51 +0100

    Minimizing the config

--- SuluAttributesBundle.php
+++ SuluAttributesBundle.php
@@ -5,13 +5,32 @@ declare(strict_types=1);
 namespace FriendsOfSulu\Bundle\SuluAttributesBundle;
 
 use FriendsOfSulu\Bundle\SuluAttributesBundle\DependencyInjection\AdminAttributeCompilerPass;
+use FriendsOfSulu\Bundle\SuluAttributesBundle\SuluOverrides\NavigationAdmin;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class SuluAttributesBundle extends Bundle
+class SuluAttributesBundle extends AbstractBundle
 {
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new AdminAttributeCompilerPass());
+    }
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services();
+
+        $services->set(NavigationAdmin::class)
+            ->args([
+                new Reference('sulu_security.security_checker'),
+                new Reference('sulu_admin.admin_pool'),
+            ])
+            ->tag('sulu.admin')
+        ;
     }
 }
